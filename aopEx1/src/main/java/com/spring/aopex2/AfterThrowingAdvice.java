c430c5f9aa38dec2f13b3cb2305a7b24180ec924
eunyoung56 <eunyoung1328@daum.net>
2022-05-19 20:27:03 +0900

    movement after-throwing joinpoint

---
+++ aopEx1/src/main/java/com/spring/aopex2/AfterThrowingAdvice.java
@@ -0,0 +1,7 @@
+package com.spring.aopex2;
+
+public class AfterThrowingAdvice {
+	public void afterThrowingAdviceLog() {
+		System.out.println("[AfterThrowingAdvice] 예외처리 발생시 실행되는 로그!!");
+	}
+}
--- aopEx1/src/main/java/com/spring/aopex2/BoardServiceImpl.java
+++ aopEx1/src/main/java/com/spring/aopex2/BoardServiceImpl.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Service;
 public class BoardServiceImpl implements BoardService{
 //	@Autowired
 //	private LogAdvice log;
+	int seq = 0;
 
 	@Override
 	public void insertBoard() {
 //		log.printLog();
 //		log.printLogModify();
 		System.out.println("[핵심관심영역 코드] insertBoard() 실행");
+		if(seq == 0) {
+			throw new IllegalArgumentException("seq가 0인 에러 발생됨!!");
+		}
 	}
 
 	@Override
